e1d975996c8a0d4888d28fab3613d0d920c2f17b
AMMAAR BAKSHI <the.ammaar.bakshi@gmail.com>
2025-10-04 23:33:48 +0530

    Create CpuBurn.java

---
+++ CpuBurn.java
@@ -0,0 +1,7 @@
+// CpuBurn.java
+public class CpuBurn {
+    public static void main(String[] args) {
+        System.out.println("Starting load...");
+        while (true) Math.log(Math.random());
+    }
+}
