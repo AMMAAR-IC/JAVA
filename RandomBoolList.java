4d1ab5bfe5546996f4e6de18d4a03ea213d018cf
AMMAAR BAKSHI <the.ammaar.ic@gmail.com>
2026-03-14 23:56:42 +0530

    Create RandomBoolList.java

---
+++ RandomBoolList.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class RandomBoolList {
+
+    public static void main(String[] args){
+
+        Random r=new Random();
+
+        for(int i=0;i<10;i++)
+            System.out.println(r.nextBoolean());
+    }
+}
