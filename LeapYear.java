ad3ff2dacc07ba58b9a234ef3687b8aa30724c45
AMMAAR BAKSHI <the.ammaar.ic@gmail.com>
2026-03-16 23:52:49 +0530

    Create LeapYear.java

---
+++ LeapYear.java
@@ -0,0 +1,13 @@
+public class LeapYear {
+
+    public static void main(String[] args){
+
+        int year = 2024;
+
+        boolean leap =
+                (year%4==0 && year%100!=0) ||
+                (year%400==0);
+
+        System.out.println(leap);
+    }
+}
