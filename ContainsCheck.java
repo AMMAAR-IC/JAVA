ba95cad7ff907731d84560c503f9ae44637a6802
AMMAAR BAKSHI <the.ammaar.ic@gmail.com>
2026-03-18 23:52:56 +0530

    Create ContainsCheck.java

---
+++ ContainsCheck.java
@@ -0,0 +1,9 @@
+public class ContainsCheck {
+
+    public static void main(String[] args){
+
+        String text = "Java programming";
+
+        System.out.println(text.contains("Java"));
+    }
+}
