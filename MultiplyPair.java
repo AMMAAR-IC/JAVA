5100fd063b1c90f88cebb69bd8d04d8b67a83205
Ammaar Bakshi <the.ammaar.ic@gmail.com>
2026-04-06 23:50:37 +0530

    Create MultiplyPair.java

---
+++ MultiplyPair.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class MultiplyPair {
+
+    public static void main(String[] args){
+
+        Random r=new Random();
+
+        int a=r.nextInt(20);
+        int b=r.nextInt(20);
+
+        System.out.println(a+" * "+b+" = "+(a*b));
+    }
+}
