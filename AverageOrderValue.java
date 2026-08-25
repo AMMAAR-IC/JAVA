7e5b7d8fd1e8a1e16020862ecd9541785d0bab14
Ammaar Bakshi <the.ammaar.ic@gmail.com>
2026-08-25 23:54:17 +0530

    Create AverageOrderValue.java

---
+++ AverageOrderValue.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.util.stream.*;
+
+record Order(
+    String customer,
+    double amount
+){}
+
+public class AverageOrderValue {
+
+    public static void main(String[] args){
+
+        List<Order> orders=
+            List.of(
+                new Order("Alex",500),
+                new Order("Alex",700),
+                new Order("Sam",300),
+                new Order("Sam",900)
+            );
+
+        Map<String,Double> result=
+
+            orders.stream()
+                  .collect(
+                      Collectors.groupingBy(
+                          Order::customer,
+                          Collectors.averagingDouble(
+                              Order::amount
+                          )
+                      )
+                  );
+
+        System.out.println(result);
+    }
+}
